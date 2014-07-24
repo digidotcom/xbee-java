3b90e31189e57164c1764e0a294d38210dc766c6
Tatiana Leon <tatiana.leon@digi.com>
2014-07-24 19:42:01 +0200

    Added the copyright to the sample source files.
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

--- examples/communication/ReceiveSerialDataSample/src/com/digi/xbee/api/receiveserialdata/MainApp.java
+++ examples/communication/ReceiveSerialDataSample/src/com/digi/xbee/api/receiveserialdata/MainApp.java
@@ -1,3 +1,14 @@
+/**
+* Copyright (c) 2014 Digi International Inc.,
+* All rights not expressly granted are reserved.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/.
+*
+* Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+* =======================================================================
+*/
 package com.digi.xbee.api.receiveserialdata;
 
 import com.digi.xbee.api.XBeeDevice;

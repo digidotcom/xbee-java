acd7354da20c51539ab889068bf7daf3992bb75b
Diego Escalona <diego.escalona@digi.com>
2014-08-01 17:07:32 +0200

    [XBJAPI-92] [XBJAPI-101] Performed several improvements to the IOSample class.
    - Added new methods to check if a specific IO line has analog or digital values.
    - Added new methods to retrieve the digital or analog value of a specific IO line.
    - IOSample class now parses the value of the power supply (if contained in the payload)
    - Added new methods to check if the sample has a power supply value and retrieve it.
    - Extended and improved the unit tests of the IOSample class.
    
    https://jira.digi.com/browse/XBJAPI-92
    https://jira.digi.com/browse/XBJAPI-101
    
    Signed-off-by: Diego Escalona <diego.escalona@digi.com>

--- src/com/digi/xbee/api/io/IOSample.java
+++ src/com/digi/xbee/api/io/IOSample.java
@@ -13,6 +13,7 @@ package com.digi.xbee.api.io;
 
 import java.util.HashMap;
 
+import com.digi.xbee.api.exceptions.XBeeException;
 import com.digi.xbee.api.utils.ByteUtils;
 
 /**
@@ -24,19 +25,19 @@ import com.digi.xbee.api.utils.ByteUtils;
  * Indicates which digital IO lines are configured in the module. Each
  * bit corresponds to one digital IO line on the module:
  * 
- * 		bit 0 = AD0/DIO0 
- * 		bit 1 = AD1/DIO1 
- * 		bit 2 = AD2/DIO2 
- * 		bit 3 = AD3/DIO3 
- * 		bit 4 = DIO4 
- * 		bit 5 = ASSOC/DIO5
- * 		bit 6 = RTS/DIO6
- * 		bit 7 = CTS/GPIO7
- * 		bit 8 = DTR / SLEEP_RQ / DIO8 
- * 		bit 9 = ON_SLEEP / DIO9 
- * 		bit 10 = RSSI/DIO10
- * 		bit 11 = PWM/DIO11
- * 		bit 12 = CD/DIO12
+ * 		bit 0 =  DIO0/AD0 
+ * 		bit 1 =  DIO1/AD1 
+ * 		bit 2 =  DIO2/AD2
+ * 		bit 3 =  DIO3/AD3
+ * 		bit 4 =  DIO4/AD4
+ * 		bit 5 =  DIO5/AD5/ASSOC
+ * 		bit 6 =  DIO6/RTS
+ * 		bit 7 =  DIO7/CTS
+ * 		bit 8 =  DIO8/DTR/SLEEP_RQ 
+ * 		bit 9 =  DIO9/ON_SLEEP 
+ * 		bit 10 = DIO10/PWM0/RSSI
+ * 		bit 11 = DIO11/PWM1
+ * 		bit 12 = DIO12/CD
  * 		bit 13 = DIO13
  * 		bit 14 = DIO14
  * 		bit 15 = N/A
@@ -54,21 +55,17 @@ import com.digi.xbee.api.utils.ByteUtils;
  * 		bit 2 = AD2/DIO2
  * 		bit 3 = AD3/DIO3
  * 		bit 4 = AD4/DIO4
- * 		bit 5 = ASSOC/AD5/DIO5
+ * 		bit 5 = AD5/DIO5/ASSOC
  * 		bit 6 = N/A
  * 		bit 7 = Supply Voltage Value
  * 
  * 		Example: mask of 0x83 means AD0, and AD1 enabled.
  * 		0 0 0 0 0 0 1 1
- * 
- * 		Analog reads are encapsulated using the AnalogRead object.
- * 
- * @see AnalogRead
  */
 public class IOSample {
 	
 	// Variables.
-	private final byte[] ioSampleBytes;
+	private final byte[] ioSamplePayload;
 	
 	private int digitalHSBMask;
 	private int digitalLSBMask;
@@ -77,24 +74,28 @@ public class IOSample {
 	private int digitalHSBValues;
 	private int digitalLSBValues;
 	private int digitalValues;
+	private int powerSupplyVoltage;
 	
 	private final HashMap<IOLine, Integer> analogValuesMap = new HashMap<IOLine, Integer>();
 	private final HashMap<IOLine, IOValue> digitalValuesMap = new HashMap<IOLine, IOValue>();
 	
 	/**
 	 * Class constructor. Instances a new object of type {@code IOSample} with the given 
-	 * IO sample byte array.
+	 * IO sample payload.
 	 * 
-	 * @param ioSampleBytes The byte array corresponding to an IO sample.
+	 * @param ioSamplePayload The payload corresponding to an IO sample.
+	 * 
+	 * @throws NullPointerException if {@code ioSamplePayload == null}.
+	 * @throws IllegalArgumentException if {@code ioSamplePayload.length < 5}.
 	 */
-	public IOSample(byte[] ioSampleBytes) {
-		if (ioSampleBytes == null)
-			throw new NullPointerException("IO sample byte array cannot be null.");
+	public IOSample(byte[] ioSamplePayload) {
+		if (ioSamplePayload == null)
+			throw new NullPointerException("IO sample payload cannot be null.");
 		
-		if (ioSampleBytes.length < 5)
-			throw new IllegalArgumentException("IO sample byte array must be longer than 4.");
+		if (ioSamplePayload.length < 5)
+			throw new IllegalArgumentException("IO sample payload must be longer than 4.");
 		
-		this.ioSampleBytes = ioSampleBytes;
+		this.ioSamplePayload = ioSamplePayload;
 		parseIOSample();
 	}
 	
@@ -105,19 +106,21 @@ public class IOSample {
 	private void parseIOSample() {
 		int dataIndex = 4;
 		
-		// Obtain the digital masks.                        // Available digital IOs
-		digitalHSBMask = ioSampleBytes[1] & 0x7F;	// 0 1 1 1 1 1 1 1
-		digitalLSBMask = ioSampleBytes[2] & 0xFF;	// 1 1 1 1 1 1 1 1
+		// Obtain the digital masks.                // Available digital IOs
+		digitalHSBMask = ioSamplePayload[1] & 0x7F;	// 0 1 1 1 1 1 1 1
+		digitalLSBMask = ioSamplePayload[2] & 0xFF;	// 1 1 1 1 1 1 1 1
 		// Combine the masks mask.
 		digitalMask = (digitalHSBMask << 8) + digitalLSBMask;
-		// Obtain the analog masks.                         // Available analog IOs
-		analogMask = ioSampleBytes[3] & 0xBF;		// 1 0 1 1 1 1 1 1
+		// Obtain the analog masks.                 // Available analog IOs
+		analogMask = ioSamplePayload[3] & 0xBF;		// 1 0 1 1 1 1 1 1
 		
 		// Read the digital values (if any). There are 16 possible digital lines.
+		// The digital mask indicates if there is any digital line enabled to read 
+		// its value. If 0, no digital values are received.
 		if (digitalMask > 0) {
 			// Obtain the digital values.
-			digitalHSBValues = ioSampleBytes[4] & 0x7F;
-			digitalLSBValues = ioSampleBytes[5] & 0xFF;
+			digitalHSBValues = ioSamplePayload[4] & 0x7F;
+			digitalLSBValues = ioSamplePayload[5] & 0xFF;
 			// Combine the values.
 			digitalValues = (digitalHSBValues << 8) + digitalLSBValues;
 			
@@ -134,18 +137,24 @@ public class IOSample {
 		}
 		
 		// Read the analog values (if any). There are 6 possible analog lines.
+		// The analog mask indicates if there is any analog line enabled to read 
+		// its value. If 0, no analog values are received.
 		int adcIndex = 0;
-		while ((ioSampleBytes.length - dataIndex) > 1 && adcIndex < 6) {
+		while ((ioSamplePayload.length - dataIndex) > 1 && adcIndex < 8) {
 			if (!ByteUtils.isBitEnabled(analogMask, adcIndex)) {
 				adcIndex += 1;
 				continue;
 			}
-			analogValuesMap.put(IOLine.getDIO(adcIndex), ((ioSampleBytes[dataIndex] & 0xFF) << 8) + (ioSampleBytes[dataIndex + 1] & 0xFF));
+			// When analog index is 7, it means that the analog value corresponds to the power 
+			// supply voltage, therefore this value should be stored in a different value.
+			if (adcIndex == 7)
+				powerSupplyVoltage = ((ioSamplePayload[dataIndex] & 0xFF) << 8) + (ioSamplePayload[dataIndex + 1] & 0xFF);
+			else
+				analogValuesMap.put(IOLine.getDIO(adcIndex), ((ioSamplePayload[dataIndex] & 0xFF) << 8) + (ioSamplePayload[dataIndex + 1] & 0xFF));
 			// Increase the data index to read the next analog values.
 			dataIndex += 2;
 			adcIndex += 1;
 		}
-		// TODO: Read supply voltage?
 	}
 	
 	/**
@@ -185,12 +194,44 @@ public class IOSample {
 	}
 	
 	/**
+	 * Retrieves whether or not the given IO line has a digital value.
+	 * 
+	 * @param ioLine The IO line to check if has a digital value.
+	 * @return {@code true} if the given IO line has a digital value, {@code false} otherwise.
+	 * 
+	 * @see IOLine
+	 */
+	public boolean hasDigitalValue(IOLine ioLine) {
+		return digitalValuesMap.containsKey(ioLine);
+	}
+	
+	/**
 	 * Retrieves the digital values map.
 	 * 
 	 * @return HashMap with the digital value of each configured IO line.
+	 * 
+	 * @see IOLine
+	 * @see IOValue
 	 */
 	public HashMap<IOLine, IOValue> getDigitalValues() {
 		return digitalValuesMap;
+	}
+	
+	/**
+	 * Retrieves the digital value of the provided IO line.
+	 * 
+	 * @param ioLine The IO line to get its digital value.
+	 * @return The IOValue of the given IO line. 
+	 * @throws IllegalArgumentException if the given IO line does not have 
+	 *                                  an associated digital value.
+	 * 
+	 * @see IOLine
+	 * @see IOValue
+	 */
+	public IOValue getDigitalValue(IOLine ioLine) {
+		if (!digitalValuesMap.containsKey(ioLine))
+			throw new IllegalArgumentException(ioLine.getName() + " does not have a digital value.");
+		return digitalValuesMap.get(ioLine);
 	}
 	
 	/**
@@ -212,11 +253,60 @@ public class IOSample {
 	}
 	
 	/**
+	 * Retrieves whether or not the given IO line has an analog value.
+	 * 
+	 * @param ioLine The IO line to check if has an analog value.
+	 * @return {@code true} if the given IO line has an analog value, {@code false} otherwise.
+	 * 
+	 * @see IOLine
+	 */
+	public boolean hasAnalogValue(IOLine ioLine) {
+		return analogValuesMap.containsKey(ioLine);
+	}
+	
+	/**
 	 * Retrieves the analog values map.
 	 * 
 	 * @return HashMap with the analog value of each configured IO line.
 	 */
 	public HashMap<IOLine, Integer> getAnalogValues() {
 		return analogValuesMap;
+	}
+	
+	/**
+	 * Retrieves the analog value of the provided IO line.
+	 * 
+	 * @param ioLine The IO line to get its analog value.
+	 * @return The analog value of the given IO line. 
+	 * @throws IllegalArgumentException if the given IO line does not have 
+	 *                                  an associated analog value.
+	 * 
+	 * @see IOLine
+	 */
+	public int getAnalogValue(IOLine ioLine) {
+		if (!analogValuesMap.containsKey(ioLine))
+			throw new IllegalArgumentException(ioLine.getName() + " does not have an analog value.");
+		return analogValuesMap.get(ioLine);
+	}
+	
+	/**
+	 * Retrieves whether or not the IOSample has power supply value.
+	 * 
+	 * @return {@code true} if the IOSample has power supply value, {@code false} otherwise.
+	 */
+	public boolean hasPowerSupplyValue() {
+		return ByteUtils.isBitEnabled(analogMask, 7);
+	}
+	
+	/**
+	 * Retrieves the value of the power supply voltage.
+	 * 
+	 * @return The value of the power supply voltage.
+	 * @throws XBeeException if the IOSample does not have power supply value.
+	 */
+	public int getPowerSupplyValue() throws XBeeException {
+		if (!ByteUtils.isBitEnabled(analogMask, 7))
+			throw new XBeeException(XBeeException.INVALID_OPERATION);
+		return powerSupplyVoltage;
 	}
 }
--- test/com/digi/xbee/api/io/IOSampleTest.java
+++ test/com/digi/xbee/api/io/IOSampleTest.java
@@ -15,6 +15,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.digi.xbee.api.exceptions.XBeeException;
+
 public class IOSampleTest {
 
 	// Constants.
@@ -27,7 +29,7 @@ public class IOSampleTest {
 	 * 
 	 * ADCs CONFIGURATION
 	 * --------------------------------------------------
-	 * DIO1 and DIO3 are configured as ADC.
+	 * DIO1 and DIO3 are configured as ADC. Power Supply voltage is provided.
 	 * 
 	 * DIGITAL I/Os VALUES
 	 * --------------------------------------------------
@@ -39,16 +41,18 @@ public class IOSampleTest {
 	 * --------------------------------------------------
 	 * DIO1 value is 0x020C (524)
 	 * DIO3 value is 0x00FA (250)
+	 * Power Supply value is 0x04E2 (1250)
 	 */
 	private static final byte[] IO_DATA_ONLY_DIGITAL = new byte[]{0x01, 0x02, 0x11, 0x00, 0x02, 0x01};
-	private static final byte[] IO_DATA_ONLY_ANALOG = new byte[]{0x01, 0x00, 0x00, 0x0A, 0x02, 0x0C, 0x00, (byte)0xFA};
-	private static final byte[] IO_DATA_MIXED = new byte[]{0x01, 0x02, 0x11, 0x0A, 0x02, 0x01, 0x02, 0x0C, 0x00, (byte)0xFA};
+	private static final byte[] IO_DATA_ONLY_ANALOG = new byte[]{0x01, 0x00, 0x00, (byte)0x8A, 0x02, 0x0C, 0x00, (byte)0xFA, 0x04, (byte)0xE2};
+	private static final byte[] IO_DATA_MIXED = new byte[]{0x01, 0x02, 0x11, (byte)0x8A, 0x02, 0x01, 0x02, 0x0C, 0x00, (byte)0xFA, 0x04, (byte)0xE2};
 	
 	private static final int DIO1_ANALOG_VALUE = 524;
 	private static final int DIO3_ANALOG_VALUE = 250;
+	private static final int POWER_SUPPLY_VALUE = 1250;
 	
 	private static final int DIGITAL_MASK = 529; // 0x0211
-	private static final int ANALOG_MASK = 10; // 0x000A
+	private static final int ANALOG_MASK = 138; // 0x8A
 	
 	@Test
 	/**
@@ -87,28 +91,66 @@ public class IOSampleTest {
 		// Verify that the analog mask is 0.
 		assertEquals(0, ioSample.getAnalogMask());
 		
+		// --------- Check Digital Values ----------------
+		
 		// Verify that the IO sample has digital values.
 		assertTrue(ioSample.hasDigitalValues());
-		
-		// Verify that the IO sample does not have analog values.
-		assertFalse(ioSample.hasAnalogValues());
 		
 		// Try to get the values of all the DIO lines, only DIO0, DIO4 and 
 		// DIO9 should have digital values, verify them.
 		for (IOLine ioLine:IOLine.values()) {
-			if (ioLine == IOLine.DIO0_AD0)
-				assertEquals(ioSample.getDigitalValues().get(ioLine), IOValue.HIGH);
-			else if (ioLine == IOLine.DIO4_AD4)
-				assertEquals(ioSample.getDigitalValues().get(ioLine), IOValue.LOW);
-			else if (ioLine == IOLine.DIO9)
-				assertEquals(ioSample.getDigitalValues().get(ioLine), IOValue.HIGH);
-			else
+			if (ioLine == IOLine.DIO0_AD0) {
+				assertTrue(ioSample.hasDigitalValue(ioLine));
+				assertEquals(IOValue.HIGH, ioSample.getDigitalValue(ioLine));
+				assertEquals(IOValue.HIGH, ioSample.getDigitalValues().get(ioLine));
+			} else if (ioLine == IOLine.DIO4_AD4) {
+				assertTrue(ioSample.hasDigitalValue(ioLine));
+				assertEquals(IOValue.LOW, ioSample.getDigitalValue(ioLine));
+				assertEquals(IOValue.LOW, ioSample.getDigitalValues().get(ioLine));
+			} else if (ioLine == IOLine.DIO9) {
+				assertTrue(ioSample.hasDigitalValue(ioLine));
+				assertEquals(IOValue.HIGH, ioSample.getDigitalValue(ioLine));
+				assertEquals(IOValue.HIGH, ioSample.getDigitalValues().get(ioLine));
+			} else {
+				assertFalse(ioSample.hasDigitalValue(ioLine));
+				try {
+					ioSample.getDigitalValue(ioLine);
+					fail("Digital value shouldn't have been retrieved.");
+				} catch (Exception e) {
+					assertEquals(IllegalArgumentException.class, e.getClass());
+				}
 				assertNull(ioSample.getDigitalValues().get(ioLine));
+			}
 		}
 		
+		// --------- Check Analog Values ----------------
+		
+		// Verify that the IO sample does not have analog values.
+		assertFalse(ioSample.hasAnalogValues());
+		
 		// Verify that there is not any analog value.
-		for (IOLine ioLine:IOLine.values())
+		for (IOLine ioLine:IOLine.values()) {
+			assertFalse(ioSample.hasAnalogValue(ioLine));
+			try {
+				ioSample.getAnalogValue(ioLine);
+				fail("Analog value shouldn't have been retrieved.");
+			} catch (Exception e) {
+				assertEquals(IllegalArgumentException.class, e.getClass());
+			}
 			assertNull(ioSample.getAnalogValues().get(ioLine));
+		}
+		
+		// -------- Check Power Supply Value ------------
+		
+		// Verify that the IO sample does not have power supply value.
+		assertFalse(ioSample.hasPowerSupplyValue());
+			
+		try {
+			ioSample.getPowerSupplyValue();
+			fail("Power supply value shouldn't have been retrieved.");
+		} catch (XBeeException e) {
+			assertEquals(XBeeException.INVALID_OPERATION, e.getErrorCode());
+		}
 	}
 	
 	@Test
@@ -122,26 +164,61 @@ public class IOSampleTest {
 		// Verify that the analog mask is correct.
 		assertEquals(ANALOG_MASK, ioSample.getAnalogMask());
 		
+		// --------- Check Digital Values ----------------
+		
 		// Verify that the IO sample does not have digital values.
 		assertFalse(ioSample.hasDigitalValues());
 		
-		// Verify that the IO sample has analog values. 
+		// Verify that there is not any digital value.
+		for (IOLine ioLine:IOLine.values()) {
+			assertFalse(ioSample.hasDigitalValue(ioLine));
+			try {
+				ioSample.getDigitalValue(ioLine);
+				fail("Digital value shouldn't have been retrieved.");
+			} catch (Exception e) {
+				assertEquals(IllegalArgumentException.class, e.getClass());
+			}
+			assertNull(ioSample.getDigitalValues().get(ioLine));
+		}
+		
+		// --------- Check Analog Values ----------------
+		
+		// Verify that the IO sample has analog values.
 		assertTrue(ioSample.hasAnalogValues());
 		
 		// Try to get the values of all the DIO lines, only DIO1 and DIO3 lines 
 		// should have analog values, verify them.
 		for (IOLine ioLine:IOLine.values()) {
-			if (ioLine == IOLine.DIO1_AD1)
+			if (ioLine == IOLine.DIO1_AD1) {
+				assertTrue(ioSample.hasAnalogValue(ioLine));
+				assertEquals(DIO1_ANALOG_VALUE, ioSample.getAnalogValue(ioLine));
 				assertEquals(DIO1_ANALOG_VALUE, (int)ioSample.getAnalogValues().get(ioLine));
-			else if (ioLine == IOLine.DIO3_AD3)
+			} else if (ioLine == IOLine.DIO3_AD3) {
+				assertTrue(ioSample.hasAnalogValue(ioLine));
+				assertEquals(DIO3_ANALOG_VALUE, ioSample.getAnalogValue(ioLine));
 				assertEquals(DIO3_ANALOG_VALUE, (int)ioSample.getAnalogValues().get(ioLine));
-			else
+			} else {
+				assertFalse(ioSample.hasAnalogValue(ioLine));
+				try {
+					ioSample.getAnalogValue(ioLine);
+					fail("Analog value shouldn't have been retrieved.");
+				} catch (Exception e) {
+					assertEquals(IllegalArgumentException.class, e.getClass());
+				}
 				assertNull(ioSample.getAnalogValues().get(ioLine));
+			}
 		}
 		
-		// Verify that there is not any digital value.
-		for (IOLine ioLine:IOLine.values())
-			assertNull(ioSample.getDigitalValues().get(ioLine));
+		// -------- Check Power Supply Value ------------
+		
+		// Verify that the IO sample has power supply value.
+		assertTrue(ioSample.hasPowerSupplyValue());
+			
+		try {
+			assertEquals(POWER_SUPPLY_VALUE, ioSample.getPowerSupplyValue());
+		} catch (XBeeException e) {
+			fail("This exception shouldn't have been thrown.");
+		}
 	}
 	
 	@Test
@@ -155,34 +232,75 @@ public class IOSampleTest {
 		// Verify that the analog mask is correct.
 		assertEquals(ANALOG_MASK, ioSample.getAnalogMask());
 		
+		// --------- Check Digital Values ----------------
+		
 		// Verify that the IO sample has digital values.
 		assertTrue(ioSample.hasDigitalValues());
-		
-		// Verify that the IO sample has analog values. 
-		assertTrue(ioSample.hasAnalogValues());
 		
 		// Try to get the values of all the DIO lines, only DIO0, DIO4 and 
 		// DIO9 should have digital values, verify them.
 		for (IOLine ioLine:IOLine.values()) {
-			if (ioLine == IOLine.DIO0_AD0)
-				assertEquals(ioSample.getDigitalValues().get(ioLine), IOValue.HIGH);
-			else if (ioLine == IOLine.DIO4_AD4)
-				assertEquals(ioSample.getDigitalValues().get(ioLine), IOValue.LOW);
-			else if (ioLine == IOLine.DIO9)
-				assertEquals(ioSample.getDigitalValues().get(ioLine), IOValue.HIGH);
-			else
+			if (ioLine == IOLine.DIO0_AD0) {
+				assertTrue(ioSample.hasDigitalValue(ioLine));
+				assertEquals(IOValue.HIGH, ioSample.getDigitalValue(ioLine));
+				assertEquals(IOValue.HIGH, ioSample.getDigitalValues().get(ioLine));
+			} else if (ioLine == IOLine.DIO4_AD4) {
+				assertTrue(ioSample.hasDigitalValue(ioLine));
+				assertEquals(IOValue.LOW, ioSample.getDigitalValue(ioLine));
+				assertEquals(IOValue.LOW, ioSample.getDigitalValues().get(ioLine));
+			} else if (ioLine == IOLine.DIO9) {
+				assertTrue(ioSample.hasDigitalValue(ioLine));
+				assertEquals(IOValue.HIGH, ioSample.getDigitalValue(ioLine));
+				assertEquals(IOValue.HIGH, ioSample.getDigitalValues().get(ioLine));
+			} else {
+				assertFalse(ioSample.hasDigitalValue(ioLine));
+				try {
+					ioSample.getDigitalValue(ioLine);
+					fail("Digital value shouldn't have been retrieved.");
+				} catch (Exception e) {
+					assertEquals(IllegalArgumentException.class, e.getClass());
+				}
 				assertNull(ioSample.getDigitalValues().get(ioLine));
+			}
 		}
+		
+		// --------- Check Analog Values ----------------
+		
+		// Verify that the IO sample has analog values.
+		assertTrue(ioSample.hasAnalogValues());
 		
 		// Try to get the values of all the DIO lines, only DIO1 and DIO3 lines 
 		// should have analog values, verify them.
 		for (IOLine ioLine:IOLine.values()) {
-			if (ioLine == IOLine.DIO1_AD1)
+			if (ioLine == IOLine.DIO1_AD1) {
+				assertTrue(ioSample.hasAnalogValue(ioLine));
+				assertEquals(DIO1_ANALOG_VALUE, ioSample.getAnalogValue(ioLine));
 				assertEquals(DIO1_ANALOG_VALUE, (int)ioSample.getAnalogValues().get(ioLine));
-			else if (ioLine == IOLine.DIO3_AD3)
+			} else if (ioLine == IOLine.DIO3_AD3) {
+				assertTrue(ioSample.hasAnalogValue(ioLine));
+				assertEquals(DIO3_ANALOG_VALUE, ioSample.getAnalogValue(ioLine));
 				assertEquals(DIO3_ANALOG_VALUE, (int)ioSample.getAnalogValues().get(ioLine));
-			else
+			} else {
+				assertFalse(ioSample.hasAnalogValue(ioLine));
+				try {
+					ioSample.getAnalogValue(ioLine);
+					fail("Analog value shouldn't have been retrieved.");
+				} catch (Exception e) {
+					assertEquals(IllegalArgumentException.class, e.getClass());
+				}
 				assertNull(ioSample.getAnalogValues().get(ioLine));
+			}
+		}
+		
+		// -------- Check Power Supply Value ------------
+		
+		// Verify that the IO sample has power supply value.
+		assertTrue(ioSample.hasPowerSupplyValue());
+			
+		try {
+			assertEquals(POWER_SUPPLY_VALUE, ioSample.getPowerSupplyValue());
+		} catch (XBeeException e) {
+			fail("This exception shouldn't have been thrown.");
 		}
 	}
 }

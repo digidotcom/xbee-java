abe239d7a241c3a3ecaf410acc5a91117acae40d
Tatiana Leon <tatiana.leon@digi.com>
2015-06-04 15:16:41 +0200

    [HardwareVersion] Added test cases for 'equals' and 'hashCode' methods.
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

--- library/src/test/java/com/digi/xbee/api/models/HardwareVersionTest.java
+++ library/src/test/java/com/digi/xbee/api/models/HardwareVersionTest.java
@@ -11,6 +11,9 @@
  */
 package com.digi.xbee.api.models;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -132,5 +135,188 @@ public class HardwareVersionTest {
 		
 		assertEquals(0, hardwareVersion.getValue());
 		assertEquals("Unknown", hardwareVersion.getDescription());
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#equals(Object)}.
+	 * 
+	 * <p>Test the equals method with a {@code null} value.</p>
+	 */
+	@Test
+	public final void testEqualsWithNull() {
+		// Setup the resources for the test.
+		HardwareVersion version = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		
+		// Call the method under test.
+		boolean areEqual = version.equals(null);
+		
+		// Verify the result.
+		assertThat("Hardware version cannot be equal to null", areEqual, is(equalTo(false)));
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#equals(Object)}.
+	 * 
+	 * <p>Test the equals method with a non {@code HardwareVersion} value.</p>
+	 */
+	@Test
+	public final void testEqualsWithNonXBeePacket() {
+		// Setup the resources for the test.
+		HardwareVersion version = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		
+		// Call the method under test.
+		boolean areEqual = version.equals(new Object());
+		
+		// Verify the result.
+		assertThat("Hardware version cannot be equal to an Object", areEqual, is(equalTo(false)));
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.packet.models.HardwareVersion(Object)}.
+	 * 
+	 * <p>Test the equals method with different {@code HardwareVersion}.</p>
+	 */
+	@Test
+	public final void testEqualsWithDifferentXBeePacket() {
+		// Setup the resources for the test.
+		HardwareVersion version1 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		HardwareVersion version2 = HardwareVersion.get(HardwareVersionEnum.X09_001.getValue());
+		
+		// Call the method under test.
+		boolean areEqual1 = version1.equals(version2);
+		boolean areEqual2 = version2.equals(version1);
+		
+		// Verify the result.
+		assertThat("Hardware version1 must be different from Hardware version2", areEqual1, is(equalTo(false)));
+		assertThat("Hardware version2 must be different from Hardware version1", areEqual2, is(equalTo(false)));
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#equals(Object)}.
+	 * 
+	 * <p>Test the equals method with equal {@code HardwareVersion}.</p>
+	 */
+	@Test
+	public final void testEqualsIsSymetric() {
+		// Setup the resources for the test.
+		HardwareVersion version1 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		HardwareVersion version2 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		
+		// Call the method under test.
+		boolean areEqual1 = version1.equals(version2);
+		boolean areEqual2 = version2.equals(version1);
+		
+		// Verify the result.
+		assertThat("Hardware version1 must be equal to Hardware version2", areEqual1, is(equalTo(true)));
+		assertThat("Hardware version2 must be equal to Hardware version1", areEqual2, is(equalTo(true)));
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#equals(Object)}.
+	 */
+	@Test
+	public final void testEqualsIsReflexive() {
+		// Setup the resources for the test.
+		HardwareVersion version = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		
+		// Call the method under test.
+		boolean areEqual = version.equals(version);
+		
+		// Verify the result.
+		assertThat("Hardware version must be equal to itself", areEqual, is(equalTo(true)));
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#equals(Object)}.
+	 */
+	@Test
+	public final void testEqualsIsTransitive() {
+		// Setup the resources for the test.
+		HardwareVersion version1 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		HardwareVersion version2 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		HardwareVersion version3 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		
+		// Call the method under test.
+		boolean areEqual1 = version1.equals(version2);
+		boolean areEqual2 = version2.equals(version3);
+		boolean areEqual3 = version1.equals(version3);
+		
+		// Verify the result.
+		assertThat("Hardware version1 must be equal to Hardware version2", areEqual1, is(equalTo(true)));
+		assertThat("Hardware version2 must be equal to Hardware version3", areEqual2, is(equalTo(true)));
+		assertThat("Hardware version1 must be equal to Hardware version3", areEqual3, is(equalTo(true)));
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#equals(Object)}.
+	 */
+	@Test
+	public final void testEqualsIsConsistent() {
+		// Setup the resources for the test.
+		HardwareVersion version1 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		HardwareVersion version2 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		HardwareVersion version3 = HardwareVersion.get(HardwareVersionEnum.X08_004.getValue());
+		
+		// Verify the result.
+		assertThat("Consistent test fail version1,version2", version1.equals(version2), is(equalTo(true)));
+		assertThat("Consistent test fail version1,version2", version1.equals(version2), is(equalTo(true)));
+		assertThat("Consistent test fail version1,version2", version1.equals(version2), is(equalTo(true)));
+		assertThat("Consistent test fail version3,version1", version3.equals(version1), is(equalTo(false)));
+		assertThat("Consistent test fail version3,version1", version3.equals(version1), is(equalTo(false)));
+		assertThat("Consistent test fail version3,version1", version3.equals(version1), is(equalTo(false)));
+
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#hashCode()}.
+	 */
+	@Test
+	public final void testHashCodeWithEqualPackets() {
+		// Setup the resources for the test.
+		HardwareVersion version1 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		HardwareVersion version2 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		
+		// Call the method under test.
+		int hashHV1 = version1.hashCode();
+		int hashHV2 = version2.hashCode();
+		
+		// Verify the result.
+		assertThat("Hardware version1 must be equal to Hardware version2", version1.equals(version2), is(equalTo(true)));
+		assertThat("Hardware version2 must be equal to Hardware version1", version2.equals(version1), is(equalTo(true)));
+		assertThat("Hash codes must be equal", hashHV1, is(equalTo(hashHV2)));
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#hashCode()}.
+	 */
+	@Test
+	public final void testHashCodeWithDifferentPackets() {
+		// Setup the resources for the test.
+		HardwareVersion version1 = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		HardwareVersion version2 = HardwareVersion.get(HardwareVersionEnum.X09_001.getValue());;
+		
+		// Call the method under test.
+		int hashHV1 = version1.hashCode();
+		int hashHV2 = version2.hashCode();
+		
+		// Verify the result.
+		assertThat("Hardware version1 must be different from Hardware version2", version1.equals(version2), is(equalTo(false)));
+		assertThat("Hardware version2 must be different from to Hardware version1", version2.equals(version1), is(equalTo(false)));
+		assertThat("Hash codes must be different", hashHV1, is(not(equalTo(hashHV2))));
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.HardwareVersion#hashCode()}.
+	 */
+	@Test
+	public final void testHashCodeIsConsistent() {
+		// Setup the resources for the test.
+		HardwareVersion version = HardwareVersion.get(HardwareVersionEnum.XBP24B.getValue());
+		
+		int initialHashCode = version.hashCode();
+		
+		// Verify the result.
+		assertThat("Consistent hashcode test fails", version.hashCode(), is(equalTo(initialHashCode)));
+		assertThat("Consistent hashcode test fails", version.hashCode(), is(equalTo(initialHashCode)));
 	}
 }

9e1e76a43d477530f8bb16139ef876945f7332ee
Diego Escalona <diego.escalona@digi.com>
2016-12-05 09:39:00 +0100

    wifi: add support to discover and connect to access points
    - Added a new enumerator (WiFiAssociationIndicationStatus) to list the
      possible encryption types for the Wi-Fi access points.
    - Added the AccessPoint class that represents an access point.
    - Added new methods to discover access points, get a specific one, connect,
      disconnect and check connection status.
    - Added new methods to get and set the Wi-Fi receive timeout, used to
      connect, disconnect and discover access points.
    - Written the unit tests for the new code and updated some others.
    
    Signed-off-by: Diego Escalona <diego.escalona@digi.com>

--- library/src/test/java/com/digi/xbee/api/WiFiDeviceTest.java
+++ library/src/test/java/com/digi/xbee/api/WiFiDeviceTest.java
@@ -13,6 +13,9 @@ package com.digi.xbee.api;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,6 +28,11 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.digi.xbee.api.connection.serial.SerialPortRxTx;
+import com.digi.xbee.api.exceptions.ATCommandException;
+import com.digi.xbee.api.exceptions.InterfaceNotOpenException;
+import com.digi.xbee.api.exceptions.InvalidOperatingModeException;
+import com.digi.xbee.api.exceptions.TimeoutException;
+import com.digi.xbee.api.exceptions.XBeeException;
 import com.digi.xbee.api.models.WiFiAssociationIndicationStatus;
 
 @RunWith(PowerMockRunner.class)
@@ -67,5 +75,141 @@ public class WiFiDeviceTest {
 		Mockito.doReturn(RESPONSE_AI).when(wifiDevice).getParameter(PARAMETER_AI);
 		
 		assertEquals(validWiFiAIStatus, wifiDevice.getWiFiAssociationIndicationStatus());
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.WiFiDevice#isConnected()}.
+	 * 
+	 * <p>Verify that it is not possible to determine if the device is connected if 
+	 * the connection of the device is closed.</p>
+	 * 
+	 * @throws XBeeException
+	 */
+	@Test(expected=InterfaceNotOpenException.class)
+	public void testIsConnectedErrorConnectionClosed() throws XBeeException {
+		// Throw an interface not open exception when getting the WiFiAssociationIndicationStatus.
+		Mockito.doThrow(new InterfaceNotOpenException()).when(wifiDevice).getWiFiAssociationIndicationStatus();
+		
+		// Check the connection.
+		wifiDevice.isConnected();
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.WiFiDevice#isConnected()}.
+	 * 
+	 * <p>Verify that it is not possible to determine if the device is connected if 
+	 * the operating mode of the device is not valid.</p>
+	 * 
+	 * @throws XBeeException
+	 */
+	@Test(expected=InvalidOperatingModeException.class)
+	public void testIsConnectedErrorInvalidOperatingMode() throws XBeeException {
+		// Throw an invalid operating mode exception when getting the WiFiAssociationIndicationStatus.
+		Mockito.doThrow(new InvalidOperatingModeException()).when(wifiDevice).getWiFiAssociationIndicationStatus();
+		
+		// Check the connection.
+		wifiDevice.isConnected();
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.WiFiDevice#isConnected()}.
+	 * 
+	 * <p>Verify that it is not possible to determine if the device is connected 
+	 * when there is an Timeout exception getting the WiFiAssociationIndicationStatus.</p>
+	 * 
+	 * @throws XBeeException
+	 * @throws IOException 
+	 */
+	@Test(expected=TimeoutException.class)
+	public void testIsConnectedErrorTimeout() throws XBeeException, IOException {
+		// Throw a timeout exception when getting the WiFiAssociationIndicationStatus.
+		Mockito.doThrow(new TimeoutException()).when(wifiDevice).getWiFiAssociationIndicationStatus();
+		
+		// Check the connection.
+		wifiDevice.isConnected();
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.WiFiDevice#isConnected()}.
+	 * 
+	 * <p>Verify that it is not possible to determine if the device is connected 
+	 * when there is an AT command exception getting the WiFiAssociationIndicationStatus.</p>
+	 * 
+	 * @throws XBeeException
+	 * @throws IOException 
+	 */
+	@Test(expected=ATCommandException.class)
+	public void testIsConnectedErrorInvalidAnswer() throws XBeeException, IOException {
+		// Throw an AT command exception when when getting the WiFiAssociationIndicationStatus.
+		Mockito.doThrow(new ATCommandException(null)).when(wifiDevice).getWiFiAssociationIndicationStatus();
+		
+		// Check the connection.
+		wifiDevice.isConnected();
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.WiFiDevice#isConnected()}.
+	 * 
+	 * <p>Verify that it is possible to determine if the device is connected or not 
+	 * when it is connected.</p>
+	 * 
+	 * @throws XBeeException
+	 * @throws IOException
+	 */
+	@Test
+	public void testIsConnectedSuccessConnected() throws XBeeException, IOException {
+		// Return the connected value when when getting the WiFiAssociationIndicationStatus.
+		Mockito.doReturn(WiFiAssociationIndicationStatus.SUCCESSFULLY_JOINED).when(wifiDevice).getWiFiAssociationIndicationStatus();
+		
+		// Check the connection.
+		assertTrue(wifiDevice.isConnected());
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.WiFiDevice#isConnected()}.
+	 * 
+	 * <p>Verify that it is possible to determine if the device is connected or not 
+	 * when it is disconnected.</p>
+	 * 
+	 * @throws XBeeException
+	 * @throws IOException
+	 */
+	@Test
+	public void testIsConnectedSuccessDisconnected() throws XBeeException, IOException {
+		// Return a valid disconnected value when when getting the WiFiAssociationIndicationStatus.
+		Mockito.doReturn(WiFiAssociationIndicationStatus.WAITING_FOR_IP).when(wifiDevice).getWiFiAssociationIndicationStatus();
+		
+		// Check the connection.
+		assertFalse(wifiDevice.isConnected());
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.WiFiDevice#setWiFiReceiveTimeout(int)}.
+	 * 
+	 * <p>Check that the Wi-Fi receive timeout cannot be set if it is negative throwing an 
+	 * {@code IllegalArgumentException}.</p>
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetWiFiReceiveTimeoutNegative() {
+		// Call the method under test.
+		wifiDevice.setWiFiReceiveTimeout(-50);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.WiFiDevice#setWiFiReceiveTimeout(int)} and 
+	 * {@link com.digi.xbee.api.WiFiDevice#getWiFiReceiveTimeout()}.
+	 * 
+	 * <p>Check that the Wi-Fi receive timeout can be set and get successfully.</p>
+	 */
+	@Test
+	public void testSetWiFiReceiveTimeoutSuccess() {
+		// First, verify that the timeout has the default value before changing it. 
+		assertEquals(15000, wifiDevice.getWiFiReceiveTimeout());
+		
+		// Call the method under test (change the timeout).
+		wifiDevice.setWiFiReceiveTimeout(5000);
+		
+		// First, verify that the new valu was set. 
+		assertEquals(5000, wifiDevice.getWiFiReceiveTimeout());
 	}
 }
---
+++ library/src/test/java/com/digi/xbee/api/models/AccessPointTest.java
@@ -0,0 +1,203 @@
+/**
+ * Copyright (c) 2016 Digi International Inc.,
+ * All rights not expressly granted are reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+ * =======================================================================
+ */
+package com.digi.xbee.api.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccessPointTest {
+
+	// Constants.
+	private static final String SSID = "My Access Point";
+	private static final WiFiEncryptionType ENCRYPTION_TYPE = WiFiEncryptionType.WEP;
+	private static final int CHANNEL = 12;
+	private static final int SIGNAL_QUALITY = 75;
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType)}.
+	 * 
+	 * <p>Verify that the {@code AccessPoint} cannot be created if the SSID is null.</p>
+	 */
+	@Test(expected=NullPointerException.class)
+	public void testCreateNullSSID() {
+		new AccessPoint(null, ENCRYPTION_TYPE);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType)}.
+	 * 
+	 * <p>Verify that the {@code AccessPoint} cannot be created if the encryption type is null.</p>
+	 */
+	@Test(expected=NullPointerException.class)
+	public void testCreateNullEncryptionType() {
+		new AccessPoint(SSID, null);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType)}.
+	 * 
+	 * <p>Verify that the {@code AccessPoint} cannot be created if the length of the SSID is 0.</p>
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateIllegalSSIDEmpty() {
+		new AccessPoint("", ENCRYPTION_TYPE);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType, int, int)}.
+	 * 
+	 * <p>Verify that the {@code AccessPoint} cannot be created if the length of the SSID is 0.</p>
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateIllegalChannelNegative() {
+		new AccessPoint(SSID, ENCRYPTION_TYPE, -5, 0);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType, int, int)}.
+	 * 
+	 * <p>Verify that the {@code AccessPoint} cannot be created if the signal quality is negative.</p>
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateIllegalSignalQualityNegative() {
+		new AccessPoint(SSID, ENCRYPTION_TYPE, CHANNEL, -10);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType, int, int)}.
+	 * 
+	 * <p>Verify that the {@code AccessPoint} cannot be created if the signal quality is 
+	 * greater than 100.</p>
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateIllegalSignalQualityBIG() {
+		new AccessPoint(SSID, ENCRYPTION_TYPE, CHANNEL, 150);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType)}, 
+	 * {@link com.digi.xbee.api.models.AccessPoint#getSSID()},
+	 * {@link com.digi.xbee.api.models.AccessPoint#getEncryptionKey()}.
+	 * 
+	 * <p>Verify that the {@code AccessPoint} can be created successfully and the getters work 
+	 * properly when the channel signal quality are not provided.</p>
+	 */
+	@Test
+	public void testCreateSuccessNotChannelSignalQuality() {
+		AccessPoint accessPoint = new AccessPoint(SSID, ENCRYPTION_TYPE);
+		
+		String accessPointString = SSID + " (" + ENCRYPTION_TYPE.name() + ") - CH: 0 - Signal: 0%";
+		
+		assertEquals(SSID, accessPoint.getSSID());
+		assertEquals(ENCRYPTION_TYPE, accessPoint.getEncryptionType());
+		assertEquals(0, accessPoint.getSignalQuality());
+		assertEquals(accessPointString, accessPoint.toString());
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType, int, int)}, 
+	 * {@link com.digi.xbee.api.models.AccessPoint#getSSID()},
+	 * {@link com.digi.xbee.api.models.AccessPoint#getEncryptionKey()},
+	 * {@link com.digi.xbee.api.models.AccessPoint#getChannel()}, 
+	 * {@link com.digi.xbee.api.models.AccessPoint#getSignalQuality()}.
+	 * 
+	 * <p>Verify that the {@code AccessPoint} can be created successfully and the getters work 
+	 * properly when the channel and signal quality are provided.</p>
+	 */
+	@Test
+	public void testCreateSuccess() {
+		AccessPoint accessPoint = new AccessPoint(SSID, ENCRYPTION_TYPE, CHANNEL, SIGNAL_QUALITY);
+		
+		String accessPointString = SSID + " (" + ENCRYPTION_TYPE.name() + ") - CH: " + CHANNEL + " - Signal: " + SIGNAL_QUALITY + "%";
+		
+		assertEquals(SSID, accessPoint.getSSID());
+		assertEquals(ENCRYPTION_TYPE, accessPoint.getEncryptionType());
+		assertEquals(CHANNEL, accessPoint.getChannel());
+		assertEquals(SIGNAL_QUALITY, accessPoint.getSignalQuality());
+		assertEquals(accessPointString, accessPoint.toString());
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType, int)}, 
+	 * {@link com.digi.xbee.api.models.AccessPoint#getSignalQuality()},
+	 * {@link com.digi.xbee.api.models.AccessPoint#setSignalQuality(int)}.
+	 * 
+	 * <p>Verify that the signal quality cannot be set if it is negative.</p>
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetSignalQualityNegative() {
+		AccessPoint accessPoint = new AccessPoint(SSID, ENCRYPTION_TYPE);
+		
+		accessPoint.setSignalQuality(-10);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType, int)}, 
+	 * {@link com.digi.xbee.api.models.AccessPoint#getSignalQuality()},
+	 * {@link com.digi.xbee.api.models.AccessPoint#setSignalQuality(int)}.
+	 * 
+	 * <p>Verify that the signal quality cannot be set if it is greater than 100.</p>
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetSignalQualityBIG() {
+		AccessPoint accessPoint = new AccessPoint(SSID, ENCRYPTION_TYPE);
+		
+		accessPoint.setSignalQuality(150);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType, int)}, 
+	 * {@link com.digi.xbee.api.models.AccessPoint#setSignalQuality(int)},
+	 * {@link com.digi.xbee.api.models.AccessPoint#getSignalQuality()}.
+	 * 
+	 * <p>Verify that the signal quality can be set and get successfully.</p>
+	 */
+	@Test
+	public void testSetSignalQualitySuccess() {
+		AccessPoint accessPoint = new AccessPoint(SSID, ENCRYPTION_TYPE);
+		
+		assertEquals(0, accessPoint.getSignalQuality());
+		accessPoint.setSignalQuality(SIGNAL_QUALITY);
+		assertEquals(SIGNAL_QUALITY, accessPoint.getSignalQuality());
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType)}, 
+	 * {@link com.digi.xbee.api.models.AccessPoint#getChannel()},
+	 * {@link com.digi.xbee.api.models.AccessPoint#setChannel(int)}.
+	 * 
+	 * <p>Verify that the channel cannot be set if it is negative.</p>
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetChannelNegative() {
+		AccessPoint accessPoint = new AccessPoint(SSID, ENCRYPTION_TYPE);
+		
+		accessPoint.setChannel(-10);
+	}
+	
+	/**
+	 * Test method for {@link com.digi.xbee.api.models.AccessPoint#AccessPoint(String, WiFiEncryptionType)}, 
+	 * {@link com.digi.xbee.api.models.AccessPoint#setChannel(int)},
+	 * {@link com.digi.xbee.api.models.AccessPoint#getChannel()}.
+	 * 
+	 * <p>Verify that the channel can be set and get successfully.</p>
+	 */
+	@Test
+	public void testSetChannelSuccess() {
+		AccessPoint accessPoint = new AccessPoint(SSID, ENCRYPTION_TYPE);
+		
+		assertEquals(0, accessPoint.getChannel());
+		accessPoint.setChannel(CHANNEL);
+		assertEquals(CHANNEL, accessPoint.getChannel());
+	}
+}

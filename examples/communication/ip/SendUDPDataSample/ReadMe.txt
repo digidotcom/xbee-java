eaafb3a58c54741d6efd3c2760f4a9ae3be8403b
Ruben Moral <Ruben.Moral@digi.com>
2017-08-08 13:27:37 +0200

    examples: add Send UDP Data example
    It is located in the 'communication/ip' category.
    
    Signed-off-by: Ruben Moral <Ruben.Moral@digi.com>

---
+++ examples/communication/ip/SendUDPDataSample/ReadMe.txt
@@ -0,0 +1,61 @@
+  Introduction
+  ------------
+  This sample Java application shows how to send UDP data from an IP device to 
+  another one connected to the Internet using the XBee Java Library.
+
+  The application sends UDP data to the specified IP address and port number.
+
+  NOTE: This example uses the NB-IoT device (NBIoTDevice) class, but it can be 
+        applied to other Internet capable XBee device classes such as 
+        CellularDevice.
+
+
+  Files
+  -----
+    * com.digi.xbee.api.sendudpdata.MainApp.java:
+      Main application class. It instantiates a NB-IoT device, establishes a 
+      serial connection with it and sends the UDP data to the specified IP 
+      address and port. Finally it prints out the result of the sent operation.
+
+
+  Requirements
+  ------------
+  To run this example you will need:
+
+    * One XBee NB-IoT radio in API mode and its corresponding carrier board 
+      (XBIB or equivalent).
+    * The XCTU application (available at www.digi.com/xctu).
+
+
+  Compatible protocols
+  --------------------
+    * Cellular
+    * Cellular NB-IoT
+    * Wi-Fi
+
+
+  Example setup
+  -------------
+    1) Plug the XBee radio into the XBee adapter and connect it to your
+       computer's USB or serial ports.
+
+    2) Ensure that the module is in API mode and connected to the Internet.
+       For further information on how to perform this task, read the 
+       'Configuring Your XBee Modules' topic of the Getting Started guide.
+
+    3) Set the port and baud rate of the XBee radio in the MainApp class.
+       If you configured the module in the previous step with XCTU, you will
+       see the port number and baud rate in the 'Port' label of the device 
+       on the left view.
+
+    4) Set the destination IP address and port number in the MainApp class.
+
+
+  Running the example
+  -------------------
+  First, build and launch the application. As soon as the application is 
+  executed, it will send the UDP packet to the specified IP address and port
+  number. If the transmission was sent succesfully, the following message will
+  be printed out in the console:
+  
+    Sending data to 192.168.1.2:9750 >> 48 65 6C 6C 6F 20 58 42 65 65 21 | Hello XBee!... Success
---
+++ examples/communication/ip/SendUDPDataSample/src/com/digi/xbee/api/sendudpdata/MainApp.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2017, Digi International Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES 
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR 
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES 
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN 
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF 
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package com.digi.xbee.api.sendudpdata;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+import com.digi.xbee.api.NBIoTDevice;
+import com.digi.xbee.api.exceptions.XBeeException;
+import com.digi.xbee.api.models.IPProtocol;
+import com.digi.xbee.api.utils.HexUtils;
+
+/**
+ * XBee Java Library Send UDP Data sample application.
+ *
+ * <p>This example sends UDP data to the specified IP address and port number.
+ * </p>
+ *
+ * <p>For a complete description on the example, refer to the 'ReadMe.txt' file
+ * included in the root directory.</p>
+ */
+public class MainApp {
+
+	/* Constants */
+
+	// TODO Replace with the serial port where your module is connected to.
+	private static final String PORT = "COM1";
+	// TODO Replace with the baud rate of your module.
+	private static final int BAUD_RATE = 9600;
+	// TODO Replace with the destination IP address.
+	private static final String DEST_IP_ADDRESS = "192.168.1.2";
+	// TODO Replace with the destination port number (in decimal format).
+	private static final int DEST_PORT = 9750;
+
+	private static final IPProtocol PROTOCOL = IPProtocol.UDP;
+
+	private static final String DATA_TO_SEND = "Hello XBee!";
+
+	/**
+	 * Application main method.
+	 *
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+		System.out.println(" +------------------------------------------+");
+		System.out.println(" |  XBee Java Library Send UDP Data Sample  |");
+		System.out.println(" +------------------------------------------+\n");
+
+		// For XBee Cellular modules, use the CellularDevice class instead.
+		NBIoTDevice myDevice = new NBIoTDevice(PORT, BAUD_RATE);
+		byte[] dataToSend = DATA_TO_SEND.getBytes();
+
+		try {
+			myDevice.open();
+
+			if (!myDevice.isConnected()) {
+				System.err.println(">> Error: the device is not connected to the network");
+				return;
+			}
+
+			System.out.format("Sending data to %s:%d >> %s | %s... ", DEST_IP_ADDRESS, DEST_PORT,
+					HexUtils.prettyHexString(HexUtils.byteArrayToHexString(dataToSend)),
+					new String(dataToSend));
+
+			myDevice.sendIPData((Inet4Address) Inet4Address.getByName(DEST_IP_ADDRESS),
+					DEST_PORT, PROTOCOL, dataToSend);
+
+			System.out.println("Success");
+
+		} catch (XBeeException | UnknownHostException e) {
+			System.out.println("Error");
+			e.printStackTrace();
+			System.exit(1);
+		} finally {
+			myDevice.close();
+		}
+	}
+}
